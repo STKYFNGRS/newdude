ca3bb76b498b8ea6f45a0d525bddcd3f8f78a400
Alex M <contact@stkyfngrs.com>
2024-12-16 19:02:53 -0800

    commit

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,20 @@ export default {
         hostname: 'cdn.shopify.com',
         pathname: '/s/files/**'
       }
-    ]
+    ],
+    minimumCacheTTL: 60,
+    // Add timeouts for image optimization
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: ['cdn.shopify.com']
+  },
+  // Add general fetch timeout
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb'
+    }
+  },
+  httpAgentOptions: {
+    keepAlive: true,
   }
 };
